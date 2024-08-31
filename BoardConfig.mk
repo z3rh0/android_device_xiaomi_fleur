2fc4e28d2ee1e981f6a9d84396504a3a6ccf1c03
claxten10 <claxten10@gmail.com>
2024-08-31 06:40:23 +0200

    fleur: Define Architecture flags
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,5 +6,17 @@
 
 DEVICE_PATH := device/xiaomi/fleur
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_VARIANT := cortex-a76
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a55
+
 # Inherit the proprietary files
 include vendor/xiaomi/fleur/BoardConfigVendor.mk
