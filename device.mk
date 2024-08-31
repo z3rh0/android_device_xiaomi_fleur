1ecad2f6c4360814fb78d1bc79827f6aa356b9c3
claxten10 <claxten10@gmail.com>
2024-08-31 07:14:14 +0200

    fleur: Configure dynamic partitions
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- device.mk
+++ device.mk
@@ -4,6 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_BUILD_SUPER_PARTITION := false
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 30
 
