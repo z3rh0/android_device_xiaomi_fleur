9901b8163a4b5a5cfe9cb716c3dba15a35735c5f
claxten10 <claxten10@gmail.com>
2024-12-26 21:58:51 +0000

    fleur: Configure GNSS stack
    Co-authored-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- device.mk
+++ device.mk
@@ -129,6 +129,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper-service.legacy
 
+# GNSS
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH) \
