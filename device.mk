fff7e325feef99e9ca2e729279afc6df9e8d22ee
claxten10 <claxten10@gmail.com>
2024-12-29 18:06:17 +0000

    fleur: Add `hardware/mediatek` to SOONG Namespaces
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- device.mk
+++ device.mk
@@ -79,7 +79,8 @@ PRODUCT_COPY_FILES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(DEVICE_PATH)
+    $(DEVICE_PATH) \
+    hardware/mediatek
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/fleur/fleur-vendor.mk)
