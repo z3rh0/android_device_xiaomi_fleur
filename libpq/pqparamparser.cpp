6567626f0861da314948b66e6ef602c490cda51c
claxten10 <claxten10@gmail.com>
2025-03-23 23:07:35 +0000

    fleur: Provide a stub libpqparamparser
    * Provide a stub libpqparamparser for HWC.
    * Required after dropping PQ stack.
    
    Signed-off-by: claxten10 <claxten10@gmail.com>

---
+++ libpq/pqparamparser.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "pqparamparser.h"
+
+int MMLPQParamParser::getMMLPQParmaAndEnableConfig(
+    mml_pq_param* param,
+    mml_pq_config* config,
+    const MMLPQHDRMetaDataInfo* hdrInfo,
+    unsigned int* someValue,
+    MMLPQ2ndOutputInfo* outputInfo
+) {
+    return 0;
+}
