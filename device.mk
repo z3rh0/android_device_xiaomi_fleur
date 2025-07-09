9c21a9ba28ef412cd73c78288f55a53327dd984a
claxten10 <claxten10@gmail.com>
2025-07-09 15:36:48 +0000

    fleur: Configure ZRAM
    * Use 40% fstab from hardware/google/pixel
    * Set swappiness to 50
    * Swap only 1 page at a time
    * Switch to LZ4 compression
    
    Change-Id: I4a6f618299b23aacede99e546c8101217a5165cc
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- device.mk
+++ device.mk
@@ -214,6 +214,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(DEVICE_PATH)/configs/media,$(TARGET_COPY_OUT_VENDOR)/etc)
 
+# MM
+PRODUCT_PACKAGES += \
+    init.zram.rc
+
+PRODUCT_COPY_FILES += \
+    hardware/google/pixel/mm/fstab.zram.40p:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.zram
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
