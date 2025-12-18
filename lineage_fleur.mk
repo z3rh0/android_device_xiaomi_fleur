5230467d2aaf097c550029d5c9d94980f946ac00
z3rh0 <serhan.uysal@hubtech.one>
2025-12-18 18:44:48 +0100

    Update lineage_fleur.mk LOS23

--- lineage_fleur.mk
+++ lineage_fleur.mk
@@ -14,11 +14,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/mainline_system.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from those products. Most specific first.
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
-
-# Inherit from device makefile.
-#$(call inherit-product, device/xiaomi/fleur/device.mk)
+# Device and Vendor trees
+$(call inherit-product, device/xiaomi/fleur/device.mk)
 
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
