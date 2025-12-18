9a9dc392d4a25cadfb2fd50f7091bc10d54dc91d
z3rh0 <serhan.uysal@hubtech.one>
2025-12-18 18:40:46 +0100

    Update lineage_fleur.mk LOS23

--- lineage_fleur.mk
+++ lineage_fleur.mk
@@ -23,7 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from device makefile.
-#$(call inherit-product, device/xiaomi/fleur/device.mk)
+$(call inherit-product, device/xiaomi/fleur/device.mk)
 
 PRODUCT_NAME := lineage_fleur
 PRODUCT_DEVICE := fleur
