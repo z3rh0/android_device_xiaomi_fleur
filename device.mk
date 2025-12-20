db48feec4efc7c24e133b8f3921bf58bc76b47a9
claxten10 <claxten10@gmail.com>
2025-12-20 01:07:30 +0000

    fleur: Build dummy libperfctl_vendor
    Change-Id: I7f81a16512a1fe45ec2df5da4f74078058101b94
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- device.mk
+++ device.mk
@@ -300,7 +300,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     libmtkperf_client_vendor:64 \
-    libpowerhalwrap_vendor:64
+    libpowerhalwrap_vendor:64 \
+    libperfctl_vendor:64
 
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
