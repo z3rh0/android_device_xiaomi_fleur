bd7c5b1ff486ac7c004919833ca30cbdf98ef79c
claxten10 <claxten10@gmail.com>
2025-12-20 01:08:43 +0000

    fleur: Build gatekeeper hal as 64-bit only
    Change-Id: I272373343f894590aad28e8409191bc81ab75baa
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- device.mk
+++ device.mk
@@ -194,7 +194,7 @@ PRODUCT_PACKAGES += \
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
-    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-impl:64 \
     android.hardware.gatekeeper@1.0-service
 
 # GNSS
